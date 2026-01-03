487120fd4ea8654db0f70124128b48b358d1e923
Naresh Y <mr.nareshy006@gmail.com>
2026-01-03 18:08:38 +0530

    Use environment variable for server port

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
